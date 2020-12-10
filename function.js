d1c006283a6bed61f0c917e59d59411fc017c6ac
Khyati Sharma <khyatisharma2001514@gmail.com>
2020-12-10 13:36:30 +0530

    retain data to maintain edit

--- function.js
+++ function.js
@@ -53,8 +53,8 @@ var App = {
     MainButtons.toggleButton(App.State.CurrentStage, true);
     PreviewPane.refresh();
   },
-  Progress(){
-    $("#completedbar").width((this.State.CurrentStage / 9 * 100) + "%" );
+  Progress() {
+    $("#completedbar").width((this.State.CurrentStage / 9 * 100) + "%");
   }
 }
 
@@ -178,7 +178,7 @@ var DataEntryPane = {
       App.UserData[App.edit] = App.UserData[this.pivot][choice];
       App.EDIT_MODE = false;
       MainButtons.toggleButton(App.State.CurrentStage, true);
-      App.showView("preview");	
+      App.showView("preview");
       PreviewPane.refresh();
     }
     else {
@@ -299,8 +299,8 @@ var DataEntryPane = {
   },
   setView(btnIndex) {
     $("#iQuestion").text(this.Questions[btnIndex]);
-    if(btnIndex==8)
-    $("#iChoices").text(App.UserData[7]);
+    if (btnIndex == 8)
+      $("#iChoices").text(App.UserData[7]);
 
   },
   showAdd() {
@@ -354,7 +354,7 @@ var DataEntryPane = {
 
 var PreviewPane = {
   "ChoicePosition": 0,
-  "textEntry":"",
+  "textEntry": "",
   refresh() {
     controls = ["#p1", "#p2", "#p3", "#p4", "#p5", "#p6", "#p7", "#p8", "#p9"];
     for (var i = 0; i < controls.length; i++) {
@@ -400,19 +400,28 @@ var PreviewPane = {
   },
   showPreview() {
     App.showView("preview");
-    this.textEntry=$.trim($("#iResponse").val());
+    this.textEntry = $.trim($("#iResponse").val());
     $("#iChoices").text("");
     $(".ibtn").hide();
     $("#SubmitResponse").show();
     $("#choiceLists").hide();
     $("#iResponse").show();
+    this.refresh();
   },
-  hidePreview(){
-    if (  App.State.CurrentStage == 1 )
+  hidePreview() {
+    if (App.State.CurrentStage == 1) {
       MainButtons.showChoicesEntryPane(App.State.CurrentStage);
-    else if ( App.State.CurrentStage == 7 )
-      MainButtons.showDecisionPane(App.State.CurrentStage);
-    else if ( App.State.CurrentStage > 1 && App.State.CurrentStage < 5 )
+      if (App.UserData[1].length > 0)
+        $("#SubmitResponse").show();
+    }
+    else if (App.State.CurrentStage == 7) {
+      $("#iResponse").hide();
+      $("#SubmitResponse").hide();
+      $("#choiceLists").show();
+      MainButtons.showDataEntryPane(App.State.CurrentStage);
+      $("#choiceLists").show();
+    }
+    else if (App.State.CurrentStage > 1 && App.State.CurrentStage < 5)
       MainButtons.showDependentEntryPane(App.State.CurrentStage);
     else
       MainButtons.showDataEntryPane(App.State.CurrentStage);
@@ -430,13 +439,19 @@ var MainButtons = {
     this.showDataEntryPane(DataEntryPane.pivot);
   },
   showDependentEntryPane(btnIndex) {
-    DataEntryPane.showNext();
+    if ((App.UserData[DataEntryPane.pivot].length - 1) == (App.UserData[App.State.CurrentStage].length)) {
+      $("#InputNextBtn").hide();
+      $("#SubmitResponse").show();
+    }
+    else
+      DataEntryPane.showNext();
     DataEntryPane.showChoices();
     this.showDataEntryPane(btnIndex);
   },
   showDecisionPane(btnIndex) {
     DataEntryPane.setDecisionPane();
     this.showDataEntryPane(btnIndex);
+    $("#choiceLists").show();
   },
   showDataEntryPane(btnIndex) {
     DataEntryPane.setView(btnIndex);
@@ -469,15 +484,15 @@ var MainButtons = {
     }
     $("#rAssess").text(App.UserData[8]);
   },
-  showDataEntryMobile(){
-    if(App.State.CurrentStage ==0){
-     this.showDataEntryPane(0);
-     $("#mobileStart").text("Continue");
+  showDataEntryMobile() {
+    if (App.State.CurrentStage == 0) {
+      this.showDataEntryPane(0);
+      $("#mobileStart").text("Continue");
     }
-    else if(App.State.CurrentStage<9){
+    else if (App.State.CurrentStage < 9) {
       App.showView("dataEntry");
     }
-    else{
+    else {
       this.showReport();
 
     }
