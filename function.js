459153f8aa185a47b51ad9612115e761e383938a
Jatin <adriano23012001@gmail.com>
2021-03-18 14:00:08 +0530

    continued with revamping code

--- function.js
+++ function.js
@@ -16,9 +16,9 @@ var helper = {
     pivot: 1,
     dependentList: [false, false, true, true, true, false, false, false, false],
     currentChoice: 0,
-    viewMap : { "preview": "#preview_area", "dataEntry": "#user_data_entry_box" },
-    viewButton : { "preview": "#show_data_entry", "dataEntry": "#show_preview" },
-    last_visible_view : "preview",
+    viewMap: { "preview": "#preview_area", "dataEntry": "#user_data_entry_box" },
+    viewButton: { "preview": "#show_data_entry", "dataEntry": "#show_preview" },
+    last_visible_view: "preview",
     questions: [
         "What is Problem ?",
         "What are the choices?",
@@ -29,8 +29,8 @@ var helper = {
         "Who Can Help ?",
         "What is your Decision ?",
         "Assess Decision"
-      ],
-      choiceTemplate(id, index, classChoice) {
+    ],
+    choiceTemplate(id, index, classChoice) {
         var tempid = id + index;
         $("#" + id + "s").append('<div class="main-block ' + classChoice + ' "  id="' + tempid + '"></div>');
         tempid = "#" + tempid;
@@ -38,36 +38,45 @@ var helper = {
         $(tempid + " .choiceContent").append('<h3>Consequences</h3><ul class = "cons"></ul><h3>Values</h3><ul class = "values"></ul><h3>Feelings</h3><ul class = "feelings"></ul>');
         var tempconsequences = this.UserData[2][index].split("\n");
         for (var i = 0; i < tempconsequences.length; i++) {
-          $(tempid + " .cons").append('<li>' + tempconsequences[i] + '</li>');
+            $(tempid + " .cons").append('<li>' + tempconsequences[i] + '</li>');
         }
         var tempvalues = this.UserData[3][index].split("\n");
         for (var i = 0; i < tempvalues.length; i++) {
-          $(tempid + " .values").append('<li>' + tempvalues[i] + '</li>');
+            $(tempid + " .values").append('<li>' + tempvalues[i] + '</li>');
         }
         var tempfeelings = this.UserData[4][index].split("\n");
         for (var i = 0; i < tempfeelings.length; i++) {
-          $(tempid + " .feelings").append('<li>' + tempfeelings[i] + '</li>');
+            $(tempid + " .feelings").append('<li>' + tempfeelings[i] + '</li>');
         }
         /*rChoices =>.main-block #rChoices0
         #rChoices0 => h1 choiceContent
         #rChoices0 .choiceContent => h3 tags +ul tags with their class
         #rChoices0 respective class=>li tags
     */
-      },
-      showView(viewName) {
+    },
+    showView(viewName) {
         $(helper.viewMap[helper.last_visible_view]).hide();
         $(helper.viewButton[helper.last_visible_view]).hide();
         helper.last_visible_view = viewName;
         $(helper.viewMap[helper.last_visible_view]).show();
         $(helper.viewButton[helper.last_visible_view]).show();
-        
-    }
+    },
+    getResponse() {
+        var response = $.trim($("#iResponse").val());
+        if (response == "") {
+            alert("Give some response");
+            $("#iResponse").focus();
+            return false;
+        }
+        return response;
+    },
 }
 
 var action = {
     showDataEntry() {
         helper.showView("dataEntry");
         dataInput.setupUserDataEntryBox();
+        $("#b" + (storageUnit.currentStage + 1)).addClass("inProgress");
     },
     showPreview() {
         $('#preview_area').show();
@@ -100,7 +109,7 @@ var general = {
 
     refresh() {
         for (var i = 0; i < storageUnit.currentStage; i++) {
-            if (helper.pivot==i || helper.dependentList[i]) {
+            if (helper.pivot == i || helper.dependentList[i]) {
                 $("#p" + (i + 1)).text(storageUnit.userData[i][helper.currentChoice]);
             }
             else {
@@ -129,9 +138,9 @@ var dataInput = {
      * 
      * 
      */
-    setupUserDataEntryBox(){
+    setupUserDataEntryBox() {
         $('#iQuestion').text(helper.questions[storageUnit.currentStage]);
-        if(storageUnit.currentStage == 7){
+        if (storageUnit.currentStage == 7) {
             $("#iResponse").hide();
             $("#iDecision").show();
             for (var i = 0; i < storageUnit.UserData[helper.pivot].length; i++) {
@@ -140,32 +149,45 @@ var dataInput = {
             }
             var addInfo = App.UserData[5].split("\n");
             for (var i = 0; i < addInfo.length; i++) {
-              $("#MoreInfo").append('<li>'+addInfo[i] + '</li>');
+                $("#MoreInfo").append('<li>' + addInfo[i] + '</li>');
             }
             var Help = App.UserData[6].split("\n");
             for (var i = 0; i < Help.length; i++) {
-              $("#Help").append('<li>'+Help[i] + '</li>');
+                $("#Help").append('<li>' + Help[i] + '</li>');
             }
         }
-        else if(storageUnit.currentStage == helper.pivot) {
-            $("#AddMore").show();
+        else if (storageUnit.currentStage == helper.pivot) {
+            $("#add_more").show();
         }
-        else if(helper.dependentList[storageUnit.currentStage]){
+        else if (helper.dependentList[storageUnit.currentStage]) {
             $("#InputNextBtn").show();
+            $("#i_choices").text(storageUnit.userData[helper.pivot][0]);
+        }
+        else {
+            $("#submit_response").show();
         }
     },
-    
-    submitResponse(){
-       /* var tempResponse = $.trim($("#iResponse").val());
-        storageUnit.userData[currentStage]=tempResponse;
-        $('#p1').val(tempResponse);
-         $('#iResponse').val("");*/
-        general.refresh();
 
-
-
-     },
-
+    submitResponse() {
+        var tempResponse = $.trim($("#iResponse").val());
+        if (this.blankChoice > 1 && tempResponse.length == 0) {
+            $("#add_more").hide();
+            $("#submit_response").hide();
+            storageUnit.CurrentStage++;
+            this.setupUserDataEntryBox();
+            general.progress();
+            $("#b" + (storageUnit.CurrentStage)).removeClass("inProgress");
+            $("#b" + (storageUnit.CurrentStage)).addClass("completed");
+            $("#b" + (storageUnit.CurrentStage + 1)).addClass("inProgress");
+            return;
+        }
+        var response = this.getResponse();
+        if (response != false) {
+            if(storageUnit.currentStage==helper.pivot || helper.dependentList[storageUnit.currentStage]){
+                
+            }
+        }
+    },
 }
 
 var TestSuite =
@@ -186,8 +208,8 @@ var TestSuite =
     },
 
     focusOnDecision() {
-        MainButtons.toggleButton(App.State.CurrentStage, false);
-        App.State.CurrentStage = 7;
+        MainButtons.toggleButton(storageUnit.CurrentStage, false);
+        storageUnit.CurrentStage = 7;
         App.UserData = [
             "focusOnDecision",
             ["choice1", "choice2", "choice3"],
@@ -202,8 +224,8 @@ var TestSuite =
         App.Beginning();
     },
     focusOnChoice() {
-        MainButtons.toggleButton(App.State.CurrentStage, false);
-        App.State.CurrentStage = 1;
+        MainButtons.toggleButton(storageUnit.CurrentStage, false);
+        storageUnit.CurrentStage = 1;
         App.UserData = [
             "focusOnChoice",
             [],
@@ -218,8 +240,8 @@ var TestSuite =
         App.Beginning();
     },
     focusOnDependentChoice() {
-        MainButtons.toggleButton(App.State.CurrentStage, false);
-        App.State.CurrentStage = 2;
+        MainButtons.toggleButton(storageUnit.CurrentStage, false);
+        storageUnit.CurrentStage = 2;
         App.UserData = [
             "focusOnDependentChoice",
             ["choice1", "choice2", "choice3"],
@@ -234,8 +256,8 @@ var TestSuite =
         App.Beginning();
     },
     focusOnAddInfo() {
-        MainButtons.toggleButton(App.State.CurrentStage, false);
-        App.State.CurrentStage = 5;
+        MainButtons.toggleButton(storageUnit.CurrentStage, false);
+        storageUnit.CurrentStage = 5;
         App.UserData = [
             "focusOnAddInfo",
             ["choice1", "choice2", "choice3"],
@@ -250,8 +272,8 @@ var TestSuite =
         App.Beginning();
     },
     focusOnSubmit() {
-        MainButtons.toggleButton(App.State.CurrentStage, false);
-        App.State.CurrentStage = 9;
+        MainButtons.toggleButton(storageUnit.CurrentStage, false);
+        storageUnit.CurrentStage = 9;
         App.UserData = [
             "focusOnSubmit",
             ["choice1", "choice2", "choice3egfgsxvdg", "choice4kd fkgkjknknknkn"],
