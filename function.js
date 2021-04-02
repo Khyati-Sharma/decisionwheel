286e65e91f129ad856396052196cf95091ace320
KHYATI SHARMA <khyatisharma2001514@gmail.com>
2021-04-02 14:04:58 +0530

    Managed code of submit response.

--- function.js
+++ function.js
@@ -91,11 +91,24 @@ var helper = {
             $("#help").append('<li>' + help[i] + '</li>');
         }
     },
-    inProgressLabel(show){
-        if(show)
-        $("#b" + (storageUnit.currentStage + 1)).addClass("in_progress");
+    inProgressLabel(show) {
+        if (show)
+            $("#b" + (storageUnit.currentStage + 1)).addClass("in_progress");
         else
-        $("#b" + (storageUnit.currentStage + 1)).removeClass("in_progress");
+            $("#b" + (storageUnit.currentStage + 1)).removeClass("in_progress");
+    },
+    submitForBlankChoice() {
+        var tempresponse = $.trim($("#i_response").val());
+        if (this.currentChoice > 1 && tempresponse == "") {
+            $("#submit_response").hide();
+            $("#add_more").hide();
+            storageUnit.currentStage++;
+            helper.currentChoice--;
+            dataInput.setupUserDataEntryBox(storageUnit.currentStage);
+            general.progress();
+            return true;
+        }
+        return false;
     }
 }
 
@@ -149,7 +162,7 @@ var action = {
             $("#r_help").append('<li>' + tempHelp[i] + '</li>')
         }
         $("#r_assess").text(storageUnit.userData[8]);
-        $("#send_report").show();
+        $("#send_report").css("display", "flex");
 
     },
     choice(choiceAction) {
@@ -176,7 +189,7 @@ var action = {
       -preservence of user_data_entry_box
     */
     sendEmail() {
-        var emailGt = $.trim($("#send_reportint").val());
+        var emailGt = $.trim($("#send_reportinp").val());
         if (emailGt == "") {
             alert("Please enter the Email");
             return;
@@ -254,7 +267,7 @@ var general = {
             }
             $("#ps" + (i + 1)).css("display", "flex");
         }
-        
+
         this.progress();
     }
 }
@@ -277,6 +290,8 @@ var dataInput = {
      * 
      */
     setupUserDataEntryBox(setupStage) {
+        if (setupStage == storageUnit.userData.length)
+            return;
         $('#i_question').text(helper.questions[setupStage]);
         $('#i_response').focus();
         if (setupStage == 7) {
@@ -319,44 +334,31 @@ var dataInput = {
     },
 
     submitResponse() {
-        var tempresponse = $.trim($("#i_response").val());
-        if (helper.currentChoice > 1 && tempresponse == "") {
-            $("#submit_response").hide();
-            $("#add_more").hide();
-            storageUnit.currentStage++;
-            this.setupUserDataEntryBox(storageUnit.currentStage);
-            general.progress();
+        if (helper.submitForBlankChoice())
             return;
-        }
         var response = helper.getResponse();
         if (response != false) {
-            $("#submit_response").hide();
             if (storageUnit.currentStage == helper.pivot || helper.dependentList[storageUnit.currentStage]) {
                 storageUnit.userData[storageUnit.currentStage][helper.currentChoice] = response;
                 $("#add_more").hide();
-                storageUnit.currentStage++;
                 helper.incr = 0;
                 $("#i_choices").hide();
-                this.setupUserDataEntryBox(storageUnit.currentStage);
-                $("#i_response").val("");
-                general.progress();
             }
-            else {
+            else
                 storageUnit.userData[storageUnit.currentStage] = response;
-                storageUnit.currentStage++;
-                general.progress();
-                if (storageUnit.currentStage == 7)
-                    helper.createDecisionView();
-                if (storageUnit.currentStage == 9) {
-                    helper.showView("preview");
-                    general.refresh();
-                    $('#show_data_entry').hide();
-                    $('#show_report').show();
-                }
-                else
-                    this.setupUserDataEntryBox(storageUnit.currentStage);
-                $('#i_response').val("");
+            $("#submit_response").hide();
+            storageUnit.currentStage++;
+            if (storageUnit.currentStage == 7)
+                helper.createDecisionView();
+            if (storageUnit.currentStage == 9) {
+                helper.showView("preview");
+                general.refresh();
+                $('#show_data_entry').hide();
+                $('#show_report').show();
             }
+            this.setupUserDataEntryBox(storageUnit.currentStage);
+            $("#i_response").val("");
+            general.progress();
         }
     },
     addChoices() {
@@ -428,7 +430,7 @@ var dataInput = {
             $("#show_preview").text("Preview");
         }
     },
-    showTemporaryData(){
+    showTemporaryData() {
         $("#i_response").val(storageUnit.tempUserData);
     }
 }
@@ -506,10 +508,10 @@ var TestSuite =
             "ADecisionf hrhf,vjjjtopwok  lcmgdllbd gdbnbnmn"
         ];
         helper.showView("preview");
-                    general.refresh();
-                    $('#show_data_entry').hide();
-                    $('#show_report').show();
+        general.refresh();
+        $('#show_data_entry').hide();
+        $('#show_report').show();
     }
-    
+
 }
 //TestSuite.focusOnSubmit();
