7e46ffd2c752a2d67c4f023d990ac4a0ef9ae671
mukeshyadav <mukeshyadav20000229@gmail.com>
2021-04-06 12:20:46 +0530

    updated file

---
+++ env.js
@@ -0,0 +1,9 @@
+var site_configuration = {
+	branding: {"name" : "Skill-Pill" },
+	title: "Decision Making Tool",
+	icon: "images/wings2life.png",
+	url: "https://ix61k6qun9.execute-api.ap-southeast-1.amazonaws.com/prod/lifetoolsdataset",
+	levelDetails: [{count:24 ,rule: "minimum" }, 
+				   {count:12, rule: "exact"},
+                   {count:6, rule: "exact"}]
+}
